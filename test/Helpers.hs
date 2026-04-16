472a17526f7dbb9abd2cd678bd4ae0cf6880a81c
StefanValentinT <stefan.v.tomaschko@icloud.com>
2026-04-16 23:56:29 +0200

    fixed tests

--- test/Helpers.hs
+++ test/Helpers.hs
@@ -17,22 +17,33 @@ assertProgramTypes input defName expectedType = do
             let typeMap = Map.fromList (Set.toList typedProgram)
             
             case Map.lookup defName typeMap of
-                Just term -> 
-                    getType term @?= expectedType
+                Just term -> do
+                    let actualType = getType term
+                    canonicalize actualType @?= canonicalize expectedType
                 Nothing -> 
-                    assertFailure $ "Definition " ++ defName ++ " not found in typed program"
+                    assertFailure $ "Definition " ++ defName ++ " not found."
 
 getType :: TypedTerm -> Type
 getType term = case term of
-    TIdent _ ty          -> ty
-    TLitInt _            -> I32
-    TApp _ _ ty          -> ty
-    TFun _ _ ty          -> ty
-    TLetIn _ _ _ ty      -> ty
-    TRecord _ ty         -> ty
-    TSel _ _ ty          -> ty
-    TIs _ _ _ _ ty       -> ty
-    TIf _ _ _ ty         -> ty
+    TIdent _ ty      -> ty
+    TLitInt _        -> I32
+    TApp _ _ ty      -> ty
+    TFun _ _ ty      -> ty
+    TLetIn _ _ _ ty  -> ty
+    TRecord _ ty     -> ty
+    TSel _ _ ty      -> ty
+    TIf _ _ _ ty     -> ty
+    TIs _ _ _ _ ty   -> ty
+
+canonicalize :: Type -> Type
+canonicalize (Union a b) = 
+    let a' = canonicalize a
+        b' = canonicalize b
+    in if show a' < show b' then Union a' b' else Union b' a'
+canonicalize (FunType a b) = FunType (canonicalize a) (canonicalize b)
+canonicalize (RecordType m) = RecordType (Map.map canonicalize m)
+canonicalize t = t
+
 
 assertProgramResult :: String -> TypedTerm -> Assertion
 assertProgramResult input expected = do
